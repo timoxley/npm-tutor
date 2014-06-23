aca2f68610099b839beea0b606bfae12ebed78af
Tim Oxley <secoif@gmail.com>
2014-06-23 15:59:18 +0800

    Create and test exercises module.

---
+++ test/exercises.js
@@ -0,0 +1,68 @@
+"use strict"
+
+var test = require('tape')
+
+var rimraf = require('rimraf')
+var mkdirp = require('mkdirp')
+
+var fs = require('fs')
+var resolve = require('path').resolve
+
+var tmpDir = require('quick-tmp')('test-exercises')
+
+var Exercises = require('../lib/exercises')
+
+function tmp() {
+  var dir = tmpDir()
+  mkdirp.sync(dir)
+  return dir
+}
+
+function writePkg(dir, pkg) {
+  var file = resolve(dir, 'package.json')
+  mkdirp.sync(dir)
+  fs.writeFileSync(file, JSON.stringify(pkg))
+  return pkg
+}
+
+test('can list all exercises in order', function(t) {
+  var dir = tmp()
+  var pkgDir1 = resolve(dir, '01-first')
+  var pkgDir2 = resolve(dir, '02-second')
+  var pkgDir3 = resolve(dir, '03-third')
+  var pkg1 = {
+    name: "first"
+  }
+  var pkg2 = {
+    name: "second"
+  }
+  var pkg3 = {
+    name: "third"
+  }
+
+  writePkg(pkgDir1, pkg1)
+  writePkg(pkgDir2, pkg2)
+  writePkg(pkgDir3, pkg3)
+
+  var exercises = Exercises(dir)
+  t.comment('ensure numeric indices work')
+  t.equal(exercises[0].name, pkg1.name)
+  t.equal(exercises[1].name, pkg2.name)
+  t.equal(exercises[2].name, pkg3.name)
+
+  t.comment('ensure length is correct')
+  t.equal(exercises.length, 3)
+
+  t.comment('ensure sequence is added')
+  t.equal(exercises[pkg1.name].sequence, 0)
+  t.equal(exercises[pkg2.name].sequence, 1)
+  t.equal(exercises[pkg3.name].sequence, 2)
+
+  t.comment('ensure pkg dir exists')
+  t.equal(exercises[pkg1.name].dir, pkgDir1)
+  t.equal(exercises[pkg2.name].dir, pkgDir2)
+  t.equal(exercises[pkg3.name].dir, pkgDir3)
+
+  rimraf.sync(dir)
+  t.end()
+})
