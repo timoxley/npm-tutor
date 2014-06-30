ae3c585c682b1f5239455f90e402f803ed2635ea
Tim Oxley <secoif@gmail.com>
2014-06-30 12:47:24 -0700

    Use workshop-exercise-help package in discovery chapter.

--- chapters/npm-workshop-discovery/help.js
+++ chapters/npm-workshop-discovery/help.js
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 
 var exercise = require('./').getCurrent()
-var resolve = require('path').resolve
-var fs = require('fs')
-var md = fs.readFileSync(resolve(exercise.dir, 'Readme.md'), 'utf8')
-md = '\n---\n' + md + '\n---'
-console.log(require('msee').parse(md, {
-  paragraphStart: '',
-  paragraphEnd: '\n\n'
-}).trimRight())
-console.log()
+console.log(require('workshop-exercise-help')(exercise))
