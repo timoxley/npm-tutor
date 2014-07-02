7b28f61080be299ea5e0dec435c35e22e15e53d0
Tim Oxley <secoif@gmail.com>
2014-07-02 16:13:14 -0700

    Print help instead of creating new subshell if already running workshop.

--- bin/npm-workshop.js
+++ bin/npm-workshop.js
@@ -3,8 +3,15 @@
 "use strict"
 
 var args = process.argv.slice(2)
-
 var workshop = require('../')
+
+if (process.env['WORKSHOP_NAME']) {
+  var help = require('../commands/helpme')
+  help.done = function() {
+    process.exit(1)
+  }
+  return help()
+}
 
 workshop(process.argv[3], function(code) {
   console.info('\nGoodbye!\n')
