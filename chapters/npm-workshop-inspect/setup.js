4d0d1fa86e7046ca30249c9a65ce7665fdbf2e28
Tim Oxley <secoif@gmail.com>
2014-07-03 13:38:16 -0700

    Improve setup to not wipe cwd.

--- chapters/npm-workshop-inspect/setup.js
+++ chapters/npm-workshop-inspect/setup.js
@@ -3,14 +3,19 @@
 if (!process.env['WORKSHOP_NAME']) return process.exit(1)
 
 var path = require('path')
-var cpr = require('cpr')
+var mkdirp = require('mkdirp')
+var rimraf = require('rimraf')
+var exec = require('child_process').exec
+
+function cpr(src, dest, fn) {
+  rimraf.sync(dest)
+  mkdirp.sync(dest)
+  exec('cp -Rp ' + src + ' ' + dest, fn)
+}
+
 var dir = process.env['WORKSHOP_WORKING_DIR']
+var lsDir = path.resolve(dir, 'npmls')
 
-var npmlsDir = path.resolve(dir, 'npmls')
-
-cpr(path.resolve(__dirname, 'bootstrap/npmls'), npmlsDir, {
-  overwrite: true, //If the file exists, overwrite it
-  confirm: true
-}, function(err, files) {
+cpr(path.resolve(__dirname, 'bootstrap/npmls/*'), lsDir, function(err) {
   if (err) throw err
 })
--- chapters/npm-workshop-prune/setup.js
+++ chapters/npm-workshop-prune/setup.js
@@ -5,46 +5,32 @@ if (!process.env['WORKSHOP_NAME']) return process.exit(1)
 var path = require('path')
 var mkdirp = require('mkdirp')
 var rimraf = require('rimraf')
-var cpr = require('cpr')
-var dir = process.env['WORKSHOP_WORKING_DIR']
-var pruneDir = path.resolve(dir, 'prune')
 var exec = require('child_process').exec
 
+function cpr(src, dest, fn) {
+  rimraf.sync(dest)
+  mkdirp.sync(dest)
+  exec('cp -Rp ' + src + ' ' + dest, fn)
+}
+
+var dir = process.env['WORKSHOP_WORKING_DIR']
+var pruneDir = path.resolve(dir, 'prune')
 var dedupeDir = path.resolve(dir, 'dedupe')
 var updateDir = path.resolve(dir, 'update')
 var shrinkwrapDir = path.resolve(dir, 'shrinkwrap')
 
-rimraf.sync(pruneDir)
-mkdirp.sync(pruneDir)
-
-cpr(path.resolve(__dirname, 'bootstrap/prune'), pruneDir, {
-  deleteFirst: true, //Delete "to" before
-  overwrite: true, //If the file exists, overwrite it
-  confirm: true
-}, function(err, files) {
+cpr(path.resolve(__dirname, 'bootstrap/prune/*'), pruneDir, function(err) {
   if (err) throw err
 })
 
-cpr(path.resolve(__dirname, 'bootstrap/dedupe'), dedupeDir, {
-  deleteFirst: true, //Delete "to" before
-  overwrite: true, //If the file exists, overwrite it
-  confirm: true
-}, function(err, files) {
+cpr(path.resolve(__dirname, 'bootstrap/dedupe/*'), dedupeDir, function(err) {
   if (err) throw err
 })
 
-cpr(path.resolve(__dirname, 'bootstrap/update'), updateDir, {
-  deleteFirst: true, //Delete "to" before
-  overwrite: true, //If the file exists, overwrite it
-  confirm: true
-}, function(err, files) {
+cpr(path.resolve(__dirname, 'bootstrap/update/*'), updateDir, function(err) {
   if (err) throw err
 })
 
-cpr(path.resolve(__dirname, 'bootstrap/shrinkwrap'), shrinkwrapDir, {
-  deleteFirst: true, //Delete "to" before
-  overwrite: true, //If the file exists, overwrite it
-  confirm: true
-}, function(err, files) {
+cpr(path.resolve(__dirname, 'bootstrap/shrinkwrap/*'), shrinkwrapDir, function(err) {
   if (err) throw err
 })
